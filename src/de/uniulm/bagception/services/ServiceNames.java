2fa2bf4ea02459e202727fda537a381ec1c19c06
Philipp Hock <mail@philipphock.de>
2013-11-05 14:40:14 +0100

    mcs name added

--- src/de/uniulm/bagception/services/ServiceNames.java
+++ src/de/uniulm/bagception/services/ServiceNames.java
@@ -5,4 +5,6 @@ public class ServiceNames {
 	public static final String BLUETOOTH_SERVER_SERVICE="de.uniulm.bagception.bluetoothserver.service.BluetoothServerService";
 	public static final String CASE_OPEN_SERVICE = "de.uniulm.bagception.service.CaseOpenService";
 	public static final String RFID_SERVICE = "de.uniulm.bagception.rfidapi.miniusbconnectionservice.service.USBConnectionService";
+	public static final String MASTER_CONTROL_SERVICE = "de.uniulm.bagception.mcs.services.MasterControlServer";
+	 
 }
