907d0c6883608256f09c18cd1b362532a81c56cc
Philipp Hock <mail@philipphock.de>
2013-11-05 13:53:45 +0100

    minime lib added

--- src/de/uniulm/bagception/services/ServiceNames.java
+++ src/de/uniulm/bagception/services/ServiceNames.java
@@ -4,5 +4,5 @@ public class ServiceNames {
 
 	public static final String BLUETOOTH_SERVER_SERVICE="de.uniulm.bagception.bluetoothserver.service.BluetoothServerService";
 	public static final String CASE_OPEN_SERVICE = "de.uniulm.bagception.service.CaseOpenService";
-	
+	public static final String RFID_SERVICE = "de.uniulm.bagception.rfidapi.miniusbconnectionservice.service.USBConnectionService";
 }
