f66b025215f5ce8b9cb62b5322f836ebb5cc7651
Philipp Hock <philipp.hock@uni-ulm.de>
2013-12-03 14:15:01 +0100

    new bc

--- src/de/uniulm/bagception/broadcastconstants/BagceptionBroadcastContants.java
+++ src/de/uniulm/bagception/broadcastconstants/BagceptionBroadcastContants.java
@@ -6,13 +6,15 @@ public interface BagceptionBroadcastContants {
 	public static final String BROADCAST_RFID_TAG_FOUND = "de.uniulm.bagception.rfid.broadcast.tagfound";
 	public static final String BROADCAST_RFID_FINISHED = "de.uniulm.bagception.rfid.broadcast.endinventory";
 	public static final String BROADCAST_RFID_NOTCONNECTED = "de.uniulm.bagception.rfid.broadcast.notconnected";
+	public static final String BROADCAST_RFID_STARTSCAN = "de.uniulm.bagception.service.broadcast.usbconnection.rfidscan";
+	public static final String BROADCAST_RFID_STOPSCAN = "de.uniulm.bagception.service.broadcast.usbconnection.rfidstopscan";
 	
 	//USB
 	public static final String USB_CONNECTION_BROADCAST_RESCAN = "de.uniulm.bagception.service.broadcast.usbconnection.rescan";
 	public static final String USB_CONNECTION_BROADCAST = "de.uniulm.bagception.service.broadcast.usbconnection";
 	public static final String USB_CONNECTION_BROADCAST_CONNECTED = "de.uniulm.bagception.service.broadcast.usbconnection.connected";
 	public static final String USB_CONNECTION_BROADCAST_DISCONNECTED = "de.uniulm.bagception.service.broadcast.usbconnection.disconnected";
-	public static final String USB_CONNECTION_BROADCAST_RFIDSCAN = "de.uniulm.bagception.service.broadcast.usbconnection.rfidscan";
+	
 	//BT
 	public static final String BROADCAST_CLIENTS_CONNECTION_UPDATE = "de.uniulm.bagception.bluetoothserver.service.BluetoothServerHandler.clientsconnectedupdate";
 	public static final String BROADCAST_CLIENTS_CONNECTION_UPDATE_REQUEST = "de.uniulm.bagception.bluetoothserver.service.BluetoothServerHandler.clientsconnectedupdaterequest";
